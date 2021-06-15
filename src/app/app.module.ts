05aa662e82eed70a2da6b76264252f8eb89d30bd
goncalveshigino <goncalveshigino20@gmail.com>
2021-06-15 23:17:20 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AccessComponent } from './access/access.component';
+import { LoginComponent } from './access/login/login.component';
+import { RegisterComponent } from './access/register/register.component';
+import { BannerComponent } from './access/banner/banner.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AccessComponent,
+    LoginComponent,
+    RegisterComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule
