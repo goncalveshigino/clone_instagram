529adbe9ad952f64f7acae75780b68191631cbc4
goncalveshigino <goncalveshigino20@gmail.com>
2021-06-16 00:22:38 +0100

    gfgfg

--- src/app/access/banner/banner.component.ts
+++ src/app/access/banner/banner.component.ts
@@ -1,15 +1,35 @@
+import { animate, state, style, transition, trigger } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-banner',
   templateUrl: './banner.component.html',
-  styleUrls: ['./banner.component.css']
+  styleUrls: ['./banner.component.css'],
+  animations: [
+    trigger('banner', [
+      state('hide', style({
+        opacity: 0
+      })),
+      state('show', style({
+        opacity: 1
+      })),
+      transition('hide <=> show', animate('2s ease-in')),
+ 
+    ])
+  ]
 })
+  
 export class BannerComponent implements OnInit {
+
+  status: string = 'hide';
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  toggleStatus(): void {
+    this.status = this.status === 'show' ? 'hide': 'show'
   }
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { AccessComponent } from './access/access.component';
@@ -16,7 +17,8 @@ import { BannerComponent } from './access/banner/banner.component';
     BannerComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
